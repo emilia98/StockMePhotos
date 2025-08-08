b15734a159363f0ebc660d7818cce2a93131e9be
emilia98 <emilia_nedialkova@abv.bg>
2025-08-08 20:47:50 +0300

    add base entity models

---
+++ StockMePhotos/StockMePhotos.Data.Models/Base/BaseDeletableModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StockMePhotos.Data.Models.Base
+{
+    public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>
+        where TKey : struct
+    {
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ StockMePhotos/StockMePhotos.Data.Models/Base/BaseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StockMePhotos.Data.Models.Base
+{
+    public abstract class BaseModel<TKey>
+        where TKey : struct
+    {
+        protected TKey Id { get; set; }
+    }
+}
