7712e1926fed726ae9aa8b855d35308b98d02161
emilia98 <emilia_nedialkova@abv.bg>
2025-08-09 19:07:49 +0300

    add Category seeding

--- StockMePhotos/StockMePhotos.Data/Configurations/CategoryConfiguration.cs
+++ StockMePhotos/StockMePhotos.Data/Configurations/CategoryConfiguration.cs
@@ -36,6 +36,53 @@ namespace StockMePhotos.Data.Configurations
                 .Property(c => c.IsDeleted)
                 .IsRequired()
                 .HasDefaultValue(false);
+
+            entity
+                .HasData(SeedCategories());
+        }
+
+        private IEnumerable<Category> SeedCategories()
+        {
+            List<Category> categoriesToSeed = new List<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Nature",
+                    Description = "Showcases the beauty of the natural world, from lush forests and flowing rivers to flowers and seasonal scenery.",
+                    Slug = "nature"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Portrait",
+                    Description = "Highlights individuals or groups with a focus on facial expressions, emotions, and personality.\r\n\r\n",
+                    Slug = "portrait"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Landscape",
+                    Description = "Captures wide and scenic views of mountains, valleys, oceans, and other breathtaking outdoor vistas.",
+                    Slug = "landscape"
+                },
+                new Category
+                {
+                    Id = 4,
+                    Name = "Animals",
+                    Description = "Features wildlife and pets in their natural habitats or playful moments, emphasizing their charm and character.",
+                    Slug = "animals"
+                },
+                new Category
+                {
+                    Id = 5,
+                    Name = "People",
+                    Description = "Depicts everyday life, candid moments, and human interactions across cultures and settings.",
+                    Slug = "people"
+                }
+            };
+
+            return categoriesToSeed;
         }
     }
 }
---
+++ StockMePhotos/StockMePhotos.Data/Migrations/20250809160622_AddCategorySeeding.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StockMePhotos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategorySeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name", "Slug" },
+                values: new object[,]
+                {
+                    { 1, "Showcases the beauty of the natural world, from lush forests and flowing rivers to flowers and seasonal scenery.", "Nature", "nature" },
+                    { 2, "Highlights individuals or groups with a focus on facial expressions, emotions, and personality.\r\n\r\n", "Portrait", "portrait" },
+                    { 3, "Captures wide and scenic views of mountains, valleys, oceans, and other breathtaking outdoor vistas.", "Landscape", "landscape" },
+                    { 4, "Features wildlife and pets in their natural habitats or playful moments, emphasizing their charm and character.", "Animals", "animals" },
+                    { 5, "Depicts everyday life, candid moments, and human interactions across cultures and settings.", "People", "people" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
