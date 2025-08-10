e36d43e64f7d4cde85f907fc3c43f76568a67139
emilia98 <emilia_nedialkova@abv.bg>
2025-08-10 22:40:22 +0300

    add simple Tag listing

---
+++ StockMePhotos/StockMePhotos.Web/Controllers/TagController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockMePhotos.Services.Core.Interfaces;
+using StockMePhotos.ViewModels.Tag;
+
+namespace StockMePhotos.Web.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagService tagService;
+
+        public TagController(ITagService tagService)
+        {
+            this.tagService = tagService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.RedirectToAction(nameof(List));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            IEnumerable<TagListViewModel> allTags = await this.tagService
+                .ListAsync();
+
+            return this.View(allTags);
+        }
+    }
+}
