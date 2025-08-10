0f2ed191c833d51026a4b4146ff0488a3d929d0f
emilia98 <emilia_nedialkova@abv.bg>
2025-08-10 19:59:41 +0300

    add simple Category listing

---
+++ StockMePhotos/StockMePhotos.ViewModels/Category/CategoryListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StockMePhotos.ViewModels.Category
+{
+    public class CategoryListViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ StockMePhotos/StockMePhotos.Web/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockMePhotos.Services.Core.Interfaces;
+using StockMePhotos.ViewModels.Category;
+
+namespace StockMePhotos.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return this.RedirectToAction(nameof(List));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            IEnumerable<CategoryListViewModel> allCategories = await this.categoryService
+                .ListAllAsync();
+
+            return this.View(allCategories);
+        }
+    }
+}
