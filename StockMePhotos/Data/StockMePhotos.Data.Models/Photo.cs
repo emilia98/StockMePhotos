5491d739474c299e7446c01587638b6ae7af55e6
emilia98 <emilia_nedialkova@abv.bg>
2026-02-15 11:22:46 +0200

    add & configure Photo Model

---
+++ StockMePhotos/Data/StockMePhotos.Data.Models/Photo.cs
@@ -0,0 +1,38 @@
+﻿using StockMePhotos.Data.Models.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static StockMePhotos.Data.Common.EntityConstants;
+using static StockMePhotos.Data.Common.DataValidation.Photo;
+
+namespace StockMePhotos.Data.Models
+{
+    public class Photo : BaseDeletableModel<Guid>
+    {
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(SlugMaxLength)]
+        public string Slug { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = DateTimeColumnType)]
+        public DateTime DateAdded { get; set; }
+
+        [Column(TypeName = DateTimeColumnType)]
+        public DateTime? LastModified { get; set; } = null;
+
+        // UserId
+
+        // IdentityUser
+
+        // PhotoUploadId?
+
+        // PhotoUpload?
+    }
+}
---
+++ StockMePhotos/Data/StockMePhotos.Data/Configurations/PhotoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockMePhotos.Data.Models;
+
+namespace StockMePhotos.Data.Configurations
+{
+    public class PhotoConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> entity)
+        {
+            entity
+                .Property(p => p.DateAdded)
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            entity
+                .Property(p => p.LastModified)
+                .HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+        }
+    }
+}
