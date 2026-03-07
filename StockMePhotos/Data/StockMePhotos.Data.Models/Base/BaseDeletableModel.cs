cec9e1406b20d0640b8c87802c42fd39196f0456
emilia98 <emilia_nedialkova@abv.bg>
2026-03-07 20:01:49 +0200

    add Base Db Models

---
+++ StockMePhotos/Data/StockMePhotos.Data.Models/Base/BaseDeletableModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockMePhotos.Data.Models.Base
+{
+    public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>
+        where TKey : struct
+    {
+        [Comment("Entity IsDeleted flag")]
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ StockMePhotos/Data/StockMePhotos.Data.Models/Base/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockMePhotos.Data.Models.Base
+{
+    public abstract class BaseModel<TKey>
+        where TKey : struct
+    {
+        [Comment("Entity ID")]
+        [Key]
+        public TKey Id { get; set; }
+    }
+}
