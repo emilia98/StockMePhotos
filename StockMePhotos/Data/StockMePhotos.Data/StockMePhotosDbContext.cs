5c15b023c11666d276f035a1ec077d5953d31ab2
emilia98 <emilia_nedialkova@abv.bg>
2026-03-07 19:58:33 +0200

    set up the DbContext

---
+++ StockMePhotos/Data/StockMePhotos.Data/StockMePhotosDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace StockMePhotos.Data
+{
+    public class StockMePhotosDbContext : DbContext
+    {
+        public StockMePhotosDbContext(DbContextOptions<StockMePhotosDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets go here
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
