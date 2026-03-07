9f86c32d046353070bbd4e988a0c31f123c22ccd
emilia98 <emilia_nedialkova@abv.bg>
2026-03-07 21:19:48 +0200

    add & configure the Tag Model

---
+++ StockMePhotos/Data/StockMePhotos.Data.Common/DataValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace StockMePhotos.Data.Common
+{
+    public static class DataValidation
+    {
+        public static class Tag
+        {
+            public const int NameMinLength = 1;
+            public const int NameMaxLength = 30;
+            public const int SlugMinLength = 1;
+            public const int SlugMaxLength = 30;
+        }
+    }
+}
---
+++ StockMePhotos/Data/StockMePhotos.Data/Migrations/20260307191855_AddTagDbModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StockMePhotos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTagDbModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Entity ID")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Slug = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Name", "Slug" },
+                values: new object[,]
+                {
+                    { 1, "Sunset", "sunset" },
+                    { 2, "Portrait", "portrait" },
+                    { 3, "Minimal", "minimal" },
+                    { 4, "Vintage", "vintage" },
+                    { 5, "Street", "street" },
+                    { 6, "Aerial", "aerial" },
+                    { 7, "Black & White", "black-white" },
+                    { 8, "Night", "night" },
+                    { 9, "Macro", "macro" },
+                    { 10, "Urban", "urban" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
