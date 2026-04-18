e5778e24e497bf37c3db67c0a957cffe6a642406
emilia98 <emilia_nedialkova@abv.bg>
2026-04-18 22:05:20 +0300

    add BaseRepository

---
+++ StockMePhotos/Data/StockMePhotos.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace StockMePhotos.Data.Repositories
+{
+    public abstract class BaseRepository : IDisposable
+    {
+        private bool isDisposed = false;
+        private readonly StockMePhotosDbContext? dbContext;
+
+        protected BaseRepository(StockMePhotosDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        protected StockMePhotosDbContext? DbContext
+            => dbContext;
+
+        protected async Task<int> SaveChangesAsync()
+        {
+            return await DbContext!.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    dbContext?.Dispose();
+                }
+            }
+
+            isDisposed = true;
+        }
+    }
+}
