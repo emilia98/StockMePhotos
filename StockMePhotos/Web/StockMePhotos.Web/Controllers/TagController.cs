2caea58f06ccb8b71eeb307b3d2ac93eadc2eb34
emilia98 <emilia_nedialkova@abv.bg>
2026-04-21 14:25:56 +0300

    add tag listing controller

---
+++ StockMePhotos/Web/StockMePhotos.Web/Controllers/TagController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockMePhotos.Services.Core.Interfaces;
+using StockMePhotos.ViewModels.Tag;
+
+namespace StockMePhotos.Web.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagService tagService;
+
+        public TagController(ITagService tagService)
+        {
+            this.tagService = tagService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<TagViewModel> allTagsViewModel = await tagService
+                .GetAllTagsOrderedByName();
+            return View(allTagsViewModel);
+        }
+    }
+}
